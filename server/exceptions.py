2472c0074e9052d9269a065caca8c5edc2f74c5b
akpandawep <djtecno3@gmail.com>
2024-02-22 14:47:47 +0530

    rundy

---
+++ server/exceptions.py
@@ -0,0 +1,10 @@
+
+    # 🥰  Thank you for helping  this journey  @LazyDeveloperr  🥰
+    # thanks Illegal Developer for Server Routing
+    #thanks @Illegal_Developer
+
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/akpandawep/Illegal-Filter-bot.git /AutoFilterWithStream
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Master-Bot
+fi
+cd /AutoFilterWithStream
+pip3 install -U -r requirements.txt
+echo "Starting...."
+python3 bot.py
